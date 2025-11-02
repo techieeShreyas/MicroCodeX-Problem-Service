3e769dfb5a83661ebc334ca570b90abeeeb8c6b0
techieShreyas <shreyasagrawaltechie@gmail.com>
2025-11-03 00:18:59 +0530

    Fix: migrated from numeric status codes to http-status-codes library

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,33 +1,51 @@
+const { StatusCodes } = require('http-status-codes');
+
 function pingProblemController(req, res){
     return res.json({message: 'Problem Controller is up.'})
 }
 
 function addProblem(req, res){
-    return res.status(501).json({
+    // return res.status(501).json({
+    //     message: "Not Implemented."
+    // });
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message: "Not Implemented."
     });
+
 }
 
 function getProblem(req, res){
-    return res.status(501).json({
+    // return res.status(501).json({
+    //     message: "Not Implemented."
+    // });
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message: "Not Implemented."
     });
 }
 
 function getProblems(req, res){
-    return res.status(501).json({
+    // return res.status(501).json({
+    //     message: "Not Implemented."
+    // });
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message: "Not Implemented."
     });
 }
 
 function deleteProblem(req, res){
-    return res.status(501).json({
+    // return res.status(501).json({
+    //     message: "Not Implemented."
+    // });
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message: "Not Implemented."
     });
 }
 
 function updateProblem(req, res){
-    return res.status(501).json({
+    // return res.status(501).json({
+    //     message: "Not Implemented."
+    // });
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message: "Not Implemented."
     });
 }
@@ -40,3 +58,11 @@ module.exports={
     updateProblem, 
     pingProblemController,
 }
+
+/**
+ * 
+ * res
+ * 
+ * res.status -> returns the same response object with status property set
+ * .json -> return the same response object which has status set but this json to be returned is also set.
+ */
