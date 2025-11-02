ad3c3b99c90a04bfbd2d1b458eb69bb8fbb75458
techieShreyas <shreyasagrawaltechie@gmail.com>
2025-11-02 23:42:03 +0530

    Fix: changed variable naming to camelCase

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.text());
 
+// If any request comes and route starts with /api, we map it to apiRouter.
 app.use('/api', apiRouter);
 
 app.get('/ping', (req, res)=>{
