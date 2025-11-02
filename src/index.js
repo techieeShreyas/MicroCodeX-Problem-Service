1e3291014246ae08316b817bc0b0908edc41cb17
techieShreyas <shreyasagrawaltechie@gmail.com>
2025-11-02 23:25:20 +0530

    Feat: Added routing structure for versioned api for problems.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ const bodyParser=require('body-parser');
 
 const { PORT }=require('./config/server.config');
 
+const apiRouter=require('./routes');
+
 const app=express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.text());
+
+app.use('/api', apiRouter);
 
 app.get('/ping', (req, res)=>{
     return res.json({message:'Problem Service is alive.'});
